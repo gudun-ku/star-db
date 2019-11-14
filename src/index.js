bdfff744a568e14946680f9b5b98f6dc2f068ba3
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-11-14 15:29:48 +0300

    Adds spinner for loading random planet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './components/app';
+import "./style.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./components/app";
+
+ReactDOM.render(<App />, document.getElementById("root"));
